6bb3622418b985053308448bc1969b266ec452dd
DioForever <dioforever001@gmail.com>
2022-12-04 18:15:36 +0100

    Code update

--- Day2-Rock Paper Scissors/day2B.py
+++ Day2-Rock Paper Scissors/day2B.py
@@ -16,9 +16,9 @@ points_all = 0
 # use the values to get the desired outcome and how many points we will be awarded for each line in input
 with open("input.txt", "r") as read:
     for hand in read:
-        # we get the number 65,66,67 for A,B,C but we want 0,1,2 so we remove 65 from each one
+        # we get the number 65,66,67 for A,B,C, but we want 0,1,2, so we remove 65 from each one
         enemy = (ord(hand.split()[0])-65)
-        # we get the number 88,89,90 for X,Y,Z but we want 0,1,2 so we remove 65 from each one
+        # we get the number 88,89,90 for X,Y,Z, but we want 0,1,2, so we remove 65 from each one
         outcome = (ord(hand.split()[1])-88)
         # we use the enemy and outcome to get the desired outcome from the list
         outcome_points = l[enemy][outcome]
