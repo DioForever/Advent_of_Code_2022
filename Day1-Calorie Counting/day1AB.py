1983920d2e1f86821e8054e54d1b16a536f1d5a9
DioForever <dioforever001@gmail.com>
2022-12-04 12:24:12 +0100

    Code update + Python 3.10

--- Day1-Calorie Counting/day1AB.py
+++ Day1-Calorie Counting/day1AB.py
@@ -4,26 +4,20 @@ with open("input.txt", "r") as read:
 #turns string into array of string into array
     for line in read:
         input.append(line.replace("\n",""))
-
 #makes array for all elves an var containing last sumed up numbers
 food = []
 last = 0
-
 #for every number in input:
 for item in input:
-
     #if space is not blank add number to LAST
     if item != '':
         last += int(item)
-
     #if space is blank add LAST to FOOD and set it up to zero
     else:
         food.append(last)
         last = 0;
-
 #sort list FOOD in ascending order
 food.sort()
-
 #print everythnig in FOOD, Max second and third max numbers and theyer sum
 print(food)
 print(f"First - {food[-1]}")
