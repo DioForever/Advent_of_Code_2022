76b2090a30023526e7d63ff1a0b0e1ea3c9a8beb
DioForever <dioforever001@gmail.com>
2022-12-04 18:15:07 +0100

    Code update

--- Day2-Rock Paper Scissors/day2B.py
+++ Day2-Rock Paper Scissors/day2B.py
@@ -1,43 +1,29 @@
-# Here I am checkin all possibilities of point awarding so i can call it and get whatever I want
-def whathand(enemy):
-    if enemy == "A":
-        win = 2+6
-        draw = 1+3
-        lose = 3
-    elif enemy  == "B":
-        win = 3+6
-        draw = 2+3
-        lose = 1
-    else:
-        win = 1 + 6
-        draw = 3 + 3
-        lose = 2
-    return win, draw, lose
+# here I create 3 lists, in each is how many points would you recieve if u were to Win, Draw, lose
+rock = [0+3, 3+1, 6+2]
+paper = [0+1, 3+2, 6+3]
+scissor = [0+2, 3+3, 6+1]
 
-# Here I am checkin if I need to lose, win or draw
-# and returning the points for the desired outcome
-def outcome_calculate(your, enemy):
-    if your == "X":
-        #you need to lose
-        points = int(whathand(enemy)[2])
-
-    elif your == "Y":
-        #you need to draw
-        points = int(whathand(enemy)[1])
-    else:
-        # you need to win
-        points = int(whathand(enemy)[0])
-    return points
-
-# created int for storing points
+# Here is the list of all the possibilities
+# and every time I select one row according to enemy hand
+# and one item in the row according if I want to win(0), draw(1), lose(2)
+l = [[rock[0],rock[1],rock[2]],
+     [paper[0],paper[1],paper[2]],
+     [scissor[0],scissor[1],scissor[2]]]
+# Here we save all the acquired points
 points_all = 0
 
-# Read input and called function (outcome_calculation)
-# and appointed points for each game and printed points_all
+# Read input and get 0,1,2 instead of A,B,C and X,Y,Z
+# use the values to get the desired outcome and how many points we will be awarded for each line in input
 with open("input.txt", "r") as read:
     for hand in read:
-        enemy = hand.split()[0]
-        outcome = hand.split()[1]
-        outcome_points = int(outcome_calculate(outcome, enemy))
+        # we get the number 65,66,67 for A,B,C but we want 0,1,2 so we remove 65 from each one
+        enemy = (ord(hand.split()[0])-65)
+        # we get the number 88,89,90 for X,Y,Z but we want 0,1,2 so we remove 65 from each one
+        outcome = (ord(hand.split()[1])-88)
+        # we use the enemy and outcome to get the desired outcome from the list
+        outcome_points = l[enemy][outcome]
+        # and we add the points to total points
         points_all += outcome_points
+# printed the points
 print(points_all)
+
