efe9a84c8683e8f11a51c100527f19ea5570e209
DioForever <dioforever001@gmail.com>
2022-12-11 16:06:56 +0100

    Day 11

---
+++ Day11-Monkey in the Middle/day11B.py
@@ -0,0 +1,46 @@
+input = open("input.txt","r").read().split("\n\n")
+monkeys = []
+inspects = [0]*len(input)
+print(inspects)
+for line in input:
+    line = line.split("\n")
+    monkey = []
+    #inventory
+    items = (line[1].split(": ")[1].split(", "))
+    items = [int(x) for x in items]
+    monkey.append(items)
+    #operation
+    monkey.append(eval(f"lambda old: {line[2].split('= ')[1]}"))
+    #test case - division
+    monkey.append(int(line[3].split("by ")[1]))
+    #true target
+    monkey.append(int(line[4].split("monkey ")[1]))
+    #false target
+    monkey.append(int(line[5].split("monkey ")[1]))
+    monkeys.append(monkey)
+
+mod = 1
+for monkey in monkeys:
+    mod *= monkey[2]
+rounds = 10000
+# for x rounds
+for r in range(rounds):
+    # for every monkey
+    monkey_number = 0
+    for monkey in monkeys:
+        for item_level in monkey[0]:
+            # for every item in monkey
+            worry_level = int(monkey[1](item_level))
+            worry_level %= mod
+            if worry_level % monkey[2] == 0:
+                monkeys[monkey[3]][0].append(worry_level)
+            else:
+                monkeys[monkey[4]][0].append(worry_level)
+            # add amount of inspects according to amount of items in inventory at the start
+            inspects[monkey_number] += len((monkey[0]))
+            # reset the inv, cuz we move all the items elsewhere
+            monkey[0] = []
+        monkey_number+=1
+inspects.sort(reverse=True)
+print(inspects)
+print(f"Bussines level {inspects[0]*inspects[1]}")
